cb13578a3901b49a1aa4fe3315be609b244d605d
Deepjyoti Das <deepdeepdasdasdas@gmail.com>
2024-04-15 13:20:42 +0530

    deploying

--- app/redux/hooks/page.js
+++ app/redux/hooks/page.js
@@ -1,5 +1,6 @@
+"use client"
 import { useDispatch, useSelector } from 'react-redux'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch()
-export const useAppSelector = useSelector() 
+export const useAppDispatch = useDispatch.withTypes()
+export const useAppSelector = useSelector.withTypes() 
